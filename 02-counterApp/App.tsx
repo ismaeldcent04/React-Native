4ba355a74910ddc49c757c242dd3a0f3ce8c78ab
ismaeldcent04 <ismadadi.26@gmail.com>
2025-09-21 19:16:36 -0400

    CounterApp finished

---
+++ 02-counterApp/App.tsx
@@ -0,0 +1,34 @@
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import FAB from "./components/FAB";
+
+export default function App() {
+  const [count, setCount] = useState(10);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textHuge}>{count}</Text>
+      <FAB
+        label="+1"
+        onLongPress={() => setCount(0)}
+        onPress={() => setCount((prevValue) => prevValue + 1)}
+        position="right"
+      />
+      <FAB label="Reset" onPress={() => setCount(0)} position="left" />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textHuge: {
+    fontSize: 120,
+    fontWeight: "100",
+  },
+});
