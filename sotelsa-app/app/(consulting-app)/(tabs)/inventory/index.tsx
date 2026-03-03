77baf7453db62885b0f6b84483881f266d170609
jsantos <jsantos@sergioguzmansoft.com>
2026-03-03 17:16:35 -0400

    Creating desing of the screens

--- sotelsa-app/app/(consulting-app)/(tabs)/inventory/index.tsx
+++ sotelsa-app/app/(consulting-app)/(tabs)/inventory/index.tsx
@@ -1,13 +1,141 @@
-import { View, Text } from "react-native";
 import React from "react";
-import { ThemedText } from "@/presentation/theme/components/themed-text";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  FlatList,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import {
+  Menu,
+  Plus,
+  Search,
+  SlidersHorizontal,
+  ChevronDown,
+  ReceiptText,
+  Boxes,
+  Wallet,
+  ShoppingCart,
+} from "lucide-react-native";
 
-const InventoryScreen = () => {
+const inventoryData = [
+  {
+    id: "1",
+    name: "Ergonomic Office Chair",
+    sku: "OC-2023",
+    stock: 45,
+    status: "in",
+    image:
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuBzMjDUSF-1eJBxVCM2xUZKndm7RC7tAFriDDZTy9LgdjZ2gsJaLeDZ7UPi41S04zPc2ELohi_e1J6AwK9MKGjXq2Fkjfq6hi3tc8HtXvUsJdmxsDdODYdiOyTe-QW5POPh2yaXavag0pPa54GD1ER0YK4wkukYqxPYkgaJ0DhUndbou48MBNtf2Du7vvd2zvTK-ADuwFWzwJCvYh7XkM0WNgFxz1zP9xGRrKa2ETMFuz4YScnSJW5FIjZxN_BQ53bEpgz0mH7B42s",
+  },
+  {
+    id: "2",
+    name: "Minimalist LED Lamp",
+    sku: "LP-99-W",
+    stock: 5,
+    status: "low",
+    image:
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuCFPZvpjgY8Hc4isNREMi_nSVgmZZCZE5iUZwQBsVwukTcSyLcwLPf5rU3ZWyN0YLB9UiOTdBHrsD1ne_s4srjHG2jd6RHttxyI-6kg5aGTFYRMxEIiNwf1JFJ-OLlFbc2VCwAcXaFW04R7djXk8Uda1s-Lym-TuIMPj6guk0JxWohuo5bpw5HQ7L6HImrth6IJFasqXb96MiExqCPiyAltEfZRZZuf13FXX86VJd2VK2R0Grk0_NZQNOj3JuxGXjC4n1c2c8tuDBg",
+  },
+  {
+    id: "3",
+    name: "HP Ink Cartridge (Black)",
+    sku: "IC-HP-01",
+    stock: 0,
+    status: "out",
+    image:
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuDjFRzeUSjhmGvkUubvO8_1h7uYhlqe0soeCnlsBhL4INBEORgj-AU-RY1ISm4n5yW0HZICntJc_NifF-nRki2vWT40ma36Wbg55gNlEYcWGTJCS6TZRQdL7LDVUy1CZTSmFZ3zRrEHY8oH0a90QBJl7IrmVRoZ4qMOQNh16PotaieN5ZekSAGX3HiUsTtQgqeF1nobbbfNt0Nkg2kTO5fyMPkY3ZMGmZy4NK1uxX2l3WyCNyMkD5LLPUaAk7xQxSAzfLOXnEgg91k",
+  },
+];
+
+export default function InventoryScreen() {
+  const renderItem = ({ item }: any) => {
+    const statusStyles =
+      item.status === "in"
+        ? "bg-green-100 text-green-700"
+        : item.status === "low"
+          ? "bg-orange-100 text-orange-700"
+          : "bg-red-100 text-red-700";
+
+    const dotColor =
+      item.status === "in"
+        ? "bg-green-500"
+        : item.status === "low"
+          ? "bg-orange-500"
+          : "bg-red-500";
+
+    return (
+      <View
+        className={`flex-row gap-4 bg-white dark:bg-gray-900 p-3 rounded-xl border border-gray-100 dark:border-gray-800 items-center ${
+          item.status === "out" ? "opacity-70" : ""
+        }`}
+      >
+        <Image
+          source={{ uri: item.image }}
+          className="w-16 h-16 rounded-lg"
+          resizeMode="cover"
+        />
+
+        <View className="flex-1">
+          <Text className="text-base font-bold text-gray-900 dark:text-white">
+            {item.name}
+          </Text>
+          <Text className="text-xs text-gray-500">SKU: {item.sku}</Text>
+
+          <View className="mt-1">
+            <Text
+              className={`text-xs font-semibold px-2 py-1 rounded-full self-start ${statusStyles}`}
+            >
+              {item.stock > 0 ? `${item.stock} in stock` : "Out of stock"}
+            </Text>
+          </View>
+        </View>
+
+        <View className={`w-2.5 h-2.5 rounded-full ${dotColor}`} />
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <ThemedText>InventoryScreen</ThemedText>
-    </View>
-  );
-};
+    <SafeAreaView className="flex-1 bg-gray-100 dark:bg-[#101922]">
+      <StatusBar style="auto" />
 
-export default InventoryScreen;
+      {/* Header */}
+      <View className="flex-row items-center justify-between p-4 border-b border-gray-200 dark:border-gray-800 bg-white dark:bg-[#101922]">
+        {/* <Menu size={24} color="#111" /> */}
+        <Text className="text-lg font-bold text-center flex-1 dark:text-white">
+          Inventory
+        </Text>
+        <TouchableOpacity className="bg-blue-100 p-2 rounded-lg">
+          <Plus size={20} color="#137fec" />
+        </TouchableOpacity>
+      </View>
+
+      {/* Search */}
+      <View className="p-4 bg-white dark:bg-[#101922] space-y-3">
+        <View className="flex-row gap-2">
+          <View className="flex-1 flex-row items-center bg-gray-100 dark:bg-gray-800 rounded-xl px-3">
+            <Search size={18} color="gray" />
+            <TextInput
+              placeholder="Search by name or SKU..."
+              className="flex-1 ml-2 text-base text-black dark:text-white"
+              placeholderTextColor="gray"
+            />
+          </View>
+        </View>
+      </View>
+
+      {/* List */}
+      <FlatList
+        data={inventoryData}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        contentContainerStyle={{ padding: 16, gap: 8 }}
+      />
+    </SafeAreaView>
+  );
+}
--- sotelsa-app/app/(consulting-app)/(tabs)/summary/index.tsx
+++ sotelsa-app/app/(consulting-app)/(tabs)/summary/index.tsx
@@ -1,24 +1,39 @@
-import { View, Text, Pressable, TextInput, ScrollView } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  Pressable,
+  TextInput,
+  ScrollView,
+  Platform,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
 import { ThemedText } from "@/presentation/theme/components/themed-text";
 import StatItem from "@/presentation/summary/components/StatItem";
 import { Ionicons } from "@expo/vector-icons";
 import DailyItem from "@/presentation/summary/components/DailyItem";
 import TabItem from "@/presentation/summary/components/TabItem";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { useAuthStore } from "@/presentation/auth/store/useAuthStore";
 
 const SummaryScreen = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const { logout } = useAuthStore();
+  const [show, setShow] = useState(false);
+  const [filtered, setFiltered] = useState(false);
+
   return (
     <View className="flex-1 bg-[#f6f7f8]">
       {/* HEADER */}
-      <View className="pt-12 pb-3 px-4 flex-row items-center justify-between bg-white border-b border-gray-100">
-        <Ionicons name="wallet" size={24} color="#111418" />
-
+      <View className="pt-16 pb-3 px-4 flex-row items-center justify-between bg-white border-b border-gray-100">
         <Text className="text-lg font-bold text-center flex-1">
           Cuadre Summary
         </Text>
 
-        <Pressable>
-          <Ionicons name="calendar-outline" size={24} color="#111418" />
+        <Pressable onPress={logout}>
+          <Ionicons name="log-out-outline" size={24} color={"blue"} />
         </Pressable>
       </View>
 
@@ -30,24 +45,58 @@ const SummaryScreen = () => {
               <Text className="text-[10px] font-bold text-gray-400 uppercase mb-1">
                 Start Date
               </Text>
-              <View className="bg-gray-100 rounded-xl px-3 py-2">
+
+              <TouchableOpacity onPress={() => setShow(true)}>
                 <TextInput
-                  placeholder="2023-10-01"
-                  className="text-sm font-medium"
+                  placeholder="Start Date"
+                  editable={false}
+                  value={
+                    startDate ? new Date(startDate).toLocaleDateString() : ""
+                  }
+                  className="bg-gray-100 rounded-xl px-3 py-2"
                 />
-              </View>
+              </TouchableOpacity>
+              {show && (
+                <DateTimePicker
+                  value={startDate}
+                  mode="date"
+                  display={Platform.OS === "ios" ? "spinner" : "default"}
+                  onChange={(event, selectedDate) => {
+                    setShow(false);
+                    if (selectedDate) {
+                      setStartDate(selectedDate);
+                    }
+                  }}
+                />
+              )}
             </View>
 
             <View className="flex-1">
               <Text className="text-[10px] font-bold text-gray-400 uppercase mb-1">
                 End Date
               </Text>
-              <View className="bg-gray-100 rounded-xl px-3 py-2">
+
+              <TouchableOpacity onPress={() => setShow(true)}>
                 <TextInput
-                  placeholder="2023-10-24"
-                  className="text-sm font-medium"
+                  placeholder="Start Date"
+                  editable={false}
+                  value={endDate ? new Date(endDate).toLocaleDateString() : ""}
+                  className="bg-gray-100 rounded-xl px-3 py-2"
                 />
-              </View>
+              </TouchableOpacity>
+              {show && (
+                <DateTimePicker
+                  value={endDate}
+                  mode="date"
+                  display={Platform.OS === "ios" ? "spinner" : "default"}
+                  onChange={(event, selectedDate) => {
+                    setShow(false);
+                    if (selectedDate) {
+                      setEndDate(selectedDate);
+                    }
+                  }}
+                />
+              )}
             </View>
           </View>
 
@@ -59,112 +108,97 @@ const SummaryScreen = () => {
           </Pressable>
         </View>
 
-        {/* SUMMARY CARD */}
-        <View className="p-4">
-          <View className="bg-white rounded-2xl p-6 border border-gray-100">
-            <View className="flex-row justify-between">
-              <View>
-                <Text className="text-xs font-bold text-gray-400 uppercase">
-                  Total Facturado
-                </Text>
-                <Text className="text-3xl font-extrabold text-[#137fec]">
-                  $12,450.00
-                </Text>
-                <Text className="text-[10px] text-gray-400 mt-1">
-                  Period: Oct 01 - Oct 24
-                </Text>
+        {filtered && (
+          <View className="p-4">
+            <View className="bg-white rounded-2xl p-6 border border-gray-100">
+              <View className="flex-row justify-between">
+                <View>
+                  <Text className="text-xs font-bold text-gray-400 uppercase">
+                    Total Facturado
+                  </Text>
+                  <Text className="text-3xl font-extrabold text-[#137fec]">
+                    $12,450.00
+                  </Text>
+                  <Text className="text-[10px] text-gray-400 mt-1">
+                    Period: Oct 01 - Oct 24
+                  </Text>
+                </View>
+
+                <View className="items-end">
+                  <Text className="text-[10px] font-bold text-gray-400 uppercase">
+                    Cant. Facturas
+                  </Text>
+                  <Text className="text-lg font-bold">104</Text>
+                </View>
               </View>
 
-              <View className="items-end">
-                <Text className="text-[10px] font-bold text-gray-400 uppercase">
-                  Cant. Facturas
+              <View className="h-px bg-gray-100 my-4" />
+
+              <View className="flex-row flex-wrap justify-between">
+                <StatItem
+                  icon="payments"
+                  color="#078838"
+                  label="Efectivo"
+                  value="$5,250.00"
+                />
+                <StatItem
+                  icon="credit-card"
+                  color="#137fec"
+                  label="Tarjeta"
+                  value="$4,380.50"
+                />
+                <StatItem
+                  icon="account-balance"
+                  color="#9333EA"
+                  label="Transferencia"
+                  value="$2,819.50"
+                />
+                <StatItem
+                  icon="pending-actions"
+                  color="#d32f2f"
+                  label="Pendiente"
+                  value="$845.00"
+                />
+              </View>
+
+              <View className="border-t border-gray-100 mt-4 pt-4 flex-row justify-between items-center">
+                <View className="flex-row items-center gap-1">
+                  <Ionicons name="cash-outline" size={16} color="#f97316" />
+                  <Text className="text-xs text-gray-500">Descuento Total</Text>
+                </View>
+
+                <Text className="text-sm font-bold text-orange-500">
+                  -$215.00
                 </Text>
-                <Text className="text-lg font-bold">104</Text>
               </View>
-            </View>
-
-            <View className="h-px bg-gray-100 my-4" />
-
-            {/* GRID */}
-            <View className="flex-row flex-wrap justify-between">
-              <StatItem
-                icon="payments"
-                color="#078838"
-                label="Efectivo"
-                value="$5,250.00"
-              />
-              <StatItem
-                icon="credit-card"
-                color="#137fec"
-                label="Tarjeta"
-                value="$4,380.50"
-              />
-              <StatItem
-                icon="account-balance"
-                color="#9333EA"
-                label="Transferencia"
-                value="$2,819.50"
-              />
-              <StatItem
-                icon="pending-actions"
-                color="#d32f2f"
-                label="Pendiente"
-                value="$845.00"
-              />
-            </View>
-
-            <View className="border-t border-gray-100 mt-4 pt-4 flex-row justify-between items-center">
-              <View className="flex-row items-center gap-1">
-                <Ionicons name="cash-outline" size={16} color="#f97316" />
-                <Text className="text-xs text-gray-500">Descuento Total</Text>
-              </View>
-
-              <Text className="text-sm font-bold text-orange-500">
-                -$215.00
-              </Text>
             </View>
           </View>
-        </View>
+        )}
 
-        {/* DAILY LIST */}
-        <View className="px-4 pb-28">
-          <Text className="text-sm font-bold mb-3">
-            Daily Balances in Period
-          </Text>
+        {filtered && (
+          <View className="px-4 pb-28">
+            <Text className="text-sm font-bold mb-3">
+              Daily Balances in Period
+            </Text>
 
-          <DailyItem
-            date="Oct 23, 2023"
-            total="$3,120.00"
-            details="28 Invoices • Closed by Admin"
-          />
-          <DailyItem
-            date="Oct 22, 2023"
-            total="$2,450.15"
-            details="21 Invoices • Closed by Cashier"
-          />
-          <DailyItem
-            date="Oct 21, 2023"
-            total="$2,980.00"
-            details="26 Invoices • Closed by Admin"
-          />
-        </View>
+            <DailyItem
+              date="Oct 23, 2023"
+              total="$3,120.00"
+              details="28 Invoices • Closed by Admin"
+            />
+            <DailyItem
+              date="Oct 22, 2023"
+              total="$2,450.15"
+              details="21 Invoices • Closed by Cashier"
+            />
+            <DailyItem
+              date="Oct 21, 2023"
+              total="$2,980.00"
+              details="26 Invoices • Closed by Admin"
+            />
+          </View>
+        )}
       </ScrollView>
-
-      {/* FLOATING BUTTON */}
-      <Pressable className="absolute bottom-24 right-6 bg-[#137fec] px-5 py-3 rounded-full shadow-lg active:scale-95">
-        <View className="flex-row items-center gap-2">
-          <Ionicons name="add" size={22} color="white" />
-          <Text className="text-white font-bold">New Cuadre</Text>
-        </View>
-      </Pressable>
-
-      {/* BOTTOM TAB */}
-      {/* <View className="absolute bottom-0 w-full bg-white border-t border-gray-200 flex-row justify-around py-3">
-        <TabItem icon="home" label="Home" active={false} />
-        <TabItem icon="account-balance-wallet" label="Cuadre" active />
-        <TabItem icon="description" label="Invoices" active={false} />
-        <TabItem icon="inventory-2" label="Inventory" active={false} />
-      </View> */}
     </View>
   );
 };
